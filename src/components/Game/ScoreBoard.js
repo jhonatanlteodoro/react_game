10fe3c64819ab896cbd2914e0a9eee5d499cbabf
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2023-02-17 11:46:38 +0100

    add score component

--- src/components/Game/ScoreBoard.js
+++ src/components/Game/ScoreBoard.js
@@ -1,32 +1,38 @@
 import React from "react";
-import GAME_STATE from "./GameState";
+import GAME_STATE from "./constants/GameState";
 
 
-class ScoreBoard extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.startGame = this.startGame.bind(this);
-        this.home = this.home.bind(this);
+const ScoreBoard = (props) => {
+
+    const startGame = () => {
+        props.handleResetScore();
+        props.handleGameStatus(GAME_STATE.PLAY);
     }
 
-    startGame(){
-        this.props.handleGameStatus(GAME_STATE.PLAY);
+    const home = () => {
+        props.handleGameStatus(GAME_STATE.HOME);
     }
 
-    home(){
-        this.props.handleGameStatus(GAME_STATE.HOME);
-    }
+    return (
 
-    render() {
-        return (
-            <>
-                <h1> Score Board</h1>
-                <button className="btn btn-primary" onClick={this.startGame}>Restart</button>
-                <button className="btn btn-secondary" onClick={this.home}>Home</button>
-            </>
-        )
-    }
+        <div className="container text-center">
+
+            <div className="card mt-5">
+                <div className="card-body">
+                    <h5 className="card-title mt-3">Score Board</h5>
+
+                    <h4 className="card-text">Thanks for playing :)</h4>
+                    <h3 className="card-text mt-5"> You did awesome {props.getScore()} points</h3>
+
+
+                </div>
+
+                <button className="btn btn-primary" onClick={startGame}>Restart</button>
+                <button className="btn btn-secondary mt-3" onClick={home}>Home</button>
+            </div>
+            
+        </div>
+    )
 }
 
 
